f99d8ac8099c9f9acea027624bd0207ae27c2979
CHINMAYA NAIK <chinmayanaik920@gmail.com>
2022-06-08 21:20:49 +0530

    5th commit

--- sso/settings.py
+++ sso/settings.py
@@ -10,8 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-fl#_9#&s=07ieq5xqk84+m%i&h2)47g5bo@w%1$30gjh-56cb='
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-fl#_9#&s=07ieq5xqk84+m%i&h2)47g5bo@w%1$30gjh-56cb='
+# SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
